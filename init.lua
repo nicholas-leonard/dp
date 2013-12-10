26fedbe9ec64ea664415a395d98dacd67919467f
Nicholas Leonard <nick@nikopia.org>
2013-12-09 22:06:58 -0500

    reorganized files into directories

--- init.lua
+++ init.lua
@@ -22,39 +22,46 @@ dp.LOG_DIR = os.getenv('DEEP_LOG_PATH')
 
 --[[ misc ]]--
 require "dp/utils"
-torch.include('dp', 'postgres.lua')
 torch.include('dp', 'multinomial.lua')
 torch.include('dp', 'choose.lua')
+torch.include('dp', 'mediator.lua')
+torch.include('dp', 'query.lua')
 
 --[[ data ]]--
-torch.include('dp', 'datatensor.lua')
-torch.include('dp', 'dataset.lua')
-torch.include('dp', 'datasource.lua')
-torch.include('dp', 'mnist.lua')
-torch.include('dp', 'preprocess.lua')
-torch.include('dp', 'batch.lua')
-torch.include('dp', 'sampler.lua')
+torch.include('dp', 'data/datatensor.lua')
+torch.include('dp', 'data/dataset.lua')
+torch.include('dp', 'data/datasource.lua')
+torch.include('dp', 'data/mnist.lua')
+torch.include('dp', 'data/preprocess.lua')
+torch.include('dp', 'data/batch.lua')
+torch.include('dp', 'data/sampler.lua')
 
 
---[[ propagation ]]--
-torch.include('dp', 'mediator.lua')
-torch.include('dp', 'propagator.lua')
-torch.include('dp', 'optimizer.lua')
-torch.include('dp', 'evaluator.lua')
-torch.include('dp', 'experiment.lua')
-torch.include('dp', 'observer.lua')
-torch.include('dp', 'logger.lua')
-torch.include('dp', 'feedback.lua')
-torch.include('dp', 'confusion.lua')
-torch.include('dp', 'visitor.lua')
+--[[ propagator ]]--
+torch.include('dp', 'propagator/propagator.lua')
+torch.include('dp', 'propagator/optimizer.lua')
+torch.include('dp', 'propagator/evaluator.lua')
+torch.include('dp', 'propagator/experiment.lua')
+
+--[[ feedback ]]--
+torch.include('dp', 'feedback/feedback.lua')
+torch.include('dp', 'feedback/confusion.lua')
+
+--[[ visitor ]]--
+torch.include('dp', 'visitor/visitor.lua')
+
+--[[ observer ]]--
+torch.include('dp', 'observer/observer.lua')
+torch.include('dp', 'observer/logger.lua')
+
 
 --[[ model ]]--
-torch.include('dp', 'model.lua')
-torch.include('dp', 'module.lua')
-torch.include('dp', 'sequential.lua')
+torch.include('dp', 'model/model.lua')
+torch.include('dp', 'model/module.lua')
+torch.include('dp', 'model/sequential.lua')
 
---[[ hyperparameter optimization ]]--
-torch.include('dp', 'hyperoptimizer.lua')
+--[[ hyper ]]--
+torch.include('dp', 'hyper/hyperoptimizer.lua')
 
 --[[ postgres ]]--
 torch.include('dp', 'postgres/postgres.lua')

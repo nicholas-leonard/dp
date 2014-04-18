b1ac1410cb697d8c497709d1b4bc6da6e53275a2
nicholas-leonard <nick@nikopia.org>
2014-04-17 20:16:57 -0400

    torch tcp tests++. Started work on Master

--- command.lua
+++ command.lua
@@ -1,4 +1,6 @@
 async = require 'async'
+local separator = '<347 SPLIT 879>'
+
 ------------------------------------------------------------------------
 --[[ Command ]]--
 -- Command design pattern
@@ -7,12 +9,12 @@ async = require 'async'
 ------------------------------------------------------------------------
 local Command = torch.class("dp.Command")
 
-function Command.__init(proxy_id)
-   self._proxy_id = proxy_id
+function Command.__init(subject_id)
+   self._subject_id = subject_id
 end
 
-function Command:proxyId()
-   return self._proxy_id
+function Command:subjectId()
+   return self._subject_id
 end
 
 function Command:execute(session)
@@ -25,7 +27,7 @@ end
 local Forward, parent = torch.class("dp.Forward", "dp.Command")
 
 function Forward:__init(model_id, input_state, carry_state, batch_state)
-   self._model_id = model_id
+   parent.init(model_id)
    self._input_state = input_state
    self._carry_state = carry_state
    self._batch_state = batch_state
@@ -33,7 +35,27 @@ end
 
 function Forward:execute(session)
    local session_map, shared_map = session:objectMaps()
-   local model = getSharedWithLocalMemento(self._model_id)
+   local model = getSharedWithLocalMemento(self._subject_id)
+   local output_state, carry_state = model:forward(
+      self._input_state, self._carry_state, self._batch_state
+   )
+   return output_state, carry_state
+end
+
+------------------------------------------------------------------------
+--[[ Locate ]]--
+-- Used by stations to query master to locate remote objects
+------------------------------------------------------------------------
+local Locate, parent = torch.class("dp.Locate", "dp.Command")
+
+function Locate:__init(master_id, addr)
+   parent.init(master_id)
+   self._addr = addr
+end
+
+function Locate:execute(session)
+   local session_map, shared_map = session:objectMaps()
+   local model = getSharedWithLocalMemento(self._subject_id)
    local output_state, carry_state = model:forward(
       self._input_state, self._carry_state, self._batch_state
    )
@@ -58,7 +80,23 @@ function ObjectMap:get(object_id)
    return self._map[object_id]
 end
 
+------------------------------------------------------------------------
+--[[ MasterProxy ]]--
+------------------------------------------------------------------------
+local MasterProxy = torch.class("dp.MasterProxy")
 
+function MasterProxy:__init(id, addr)
+   self._id = id
+   self._addr = addr
+end
+
+function MasterProxy:id()
+   return self._id
+end
+
+function MasterProxy:addr()
+   return self._addr
+end
 
 ------------------------------------------------------------------------
 --[[ Station ]]--
@@ -67,11 +105,33 @@ end
 local Station = torch.class("dp.Station")
 
 function Station:__init(config)
+   local args, master_proxy, directory = xlua.unpack(
+      {config or {}}
+      'Station', nil
+      {arg='master_proxy', type='dp.MasterProxy', req=true,
+       help='proxy of the Master process object'},
+      {arg='directory', type='table'}
+   )
+   self._directory = directory or {}
+   self._master_proxy = master_proxy
+end
+
+function Station:locate(object_id)
+   local addr = self._directory[object_id]
+   if not addr then
+      local locate = dp.Locate(self._master_proxy:id(), addr)
+      self:_send(locate, self._master_proxy:addr())
+   end
 end
 
 function Station:send(command, session_id)
-   local destination = self:locate(command:id())
-   async.tcp.client(
+   local addr = self:locate(command:subjectId())
+   command:setSessionId(session_id)
+   self._send(command, addr)
+end
+
+function Station:_send(command, dest_addr)
+   
 end
 
 
@@ -100,8 +160,8 @@ function Session:__init(config)
    )
    self._id = id
    self._station = station
-   self._local_map = local_map
-   self._shared_map = shared_map
+   self._local_map = local_map or dp.ObjectMap
+   self._shared_map = shared_map or dp.ObjectMap
 end
 
 function Session:getSharedWithLocalMemento(object_id)
@@ -122,7 +182,7 @@ end
 ------------------------------------------------------------------------
 --[[ ModelProxy ]]--
 ------------------------------------------------------------------------
-local ModelProxy, parent = torch.class("dp.ModelProxy", "dp.Model")
+local ModelProxy, parent = torch.class("dp.ModelProxy", "dp.BaseModel")
 
 function ModelProxy:forward(input_state, carry_state, batch_state)
    -- build Forward command
@@ -133,7 +193,7 @@ end
 ------------------------------------------------------------------------
 --[[ RemoteModel ]]--
 ------------------------------------------------------------------------
-local RemoteModel, parent = torch.class("dp.RemoteModel", "dp.Model")
+local RemoteModel, parent = torch.class("dp.RemoteModel", "dp.BaseModel")
 
 function RemoteModel:__init(config)
    local args, station
@@ -154,3 +214,62 @@ function RemoteModel:setMemento(memento)
    self._state = memento:getState()
 end
 
+
+------------------------------------------------------------------------
+--[[ Master ]]--
+-- Station
+-- An object representing the master process of the system
+------------------------------------------------------------------------
+local Master, parent = torch.class("dp.Master", "dp.Station")
+
+function Master:__init(config)
+   local args, id, serial_mode = xlua.unpack(
+      {config or {}},
+      'Master', 
+      'Represents the master process of the system.'
+      {arg='id', type='dp.ObjectID', req=true},
+      {arg='serial_mode', type='string', default='ascii'}
+   )
+   self._id = id
+   self._mode = mode
+   -- contains all sessions
+   self._session_map = {}
+   -- contains all master proxies (slaves)
+   self._slave_map = {}
+   
+   self._server = async.tcp.listen({host='localhost', port=8483}, function(client)
+      print('new connection:',client)
+      client.onsplitdata(separator, function(data)
+         print('received #data :', #data)
+         local command = torch.deserialize(data, self._mode)
+         local session = self:session(command:sessionId())
+      end)
+      client.onend(function()
+         print('client ended')
+      end)
+      client.onclose(function()
+         print('closed.')
+         collectgarbage()
+         print(collectgarbage("count") * 1024)
+      end)
+   end)
+end
+
+function Master:newSession()
+   return Session{station=
+end
+
+function Master:session(session_id)
+   return self._session_map[session_id]
+end
+
+------------------------------------------------------------------------
+--[[ AsyncExperiment ]]--
+-- A distributed asynchronous experiment
+------------------------------------------------------------------------
+local AsyncExperiment, parent = torch.class("dp.AsyncExperiment", "dp.Experiment")
+
+function AsyncExperiment:setup(config)
+   self._master = config.master or assert(false)
+   parent.setup(self, config)
+end
--- test/test-tcp-client.lua
+++ test/test-tcp-client.lua
@@ -2,33 +2,59 @@ require 'torch'
 async = require 'async'
 
 local separator = '<347 SPLIT 879>'
+local mode = 'binary'
+local sent = 0
+local recv = 0
+local nClients = 10
 
-local tensor = torch.randperm(400000):resize(400,1000)
-local data = torch.serialize(tensor, 'ascii')
+local tensor = torch.randn(8,1100)
+
+clients = {}
 
 -- session (tcp client)
-local client = async.tcp.connect('tcp://localhost:8483/', function(client)
-   print('new connection:',client)
-   client.onsplitdata(separator, function(data)
-      print('received:', #data)
+for i = 1,nClients do
+   local client = async.tcp.connect('tcp://localhost:8483/', function(client)
+      print('new connection:',client)
+      client.onsplitdata(separator, function(data)
+         print('received:', #data)
+         local tensor = torch.deserialize(data, mode)
+         print('received tensor :', tensor:size())
+         recv = recv+1
+         data = torch.serialize(tensor, mode)
+         print('sending data', #data)
+         client.write(data)
+         client.write(separator)
+         sent=sent+1
+      end)
+      client.onend(function()
+         print('client ended')
+      end)
+      client.onclose(function()
+         print('closed.')
+      end)
+      --client.write('test')
+
+      
+      local data = torch.serialize(tensor, mode)
+      print('sending :', #data)
       client.write(data)
       client.write(separator)
-   end)
-   client.onend(function()
-      print('client ended')
-   end)
-   client.onclose(function()
-      print('closed.')
-   end)
-   --client.write('test')
+      sent=sent+1
 
-   print('sending :', #data)
-   client.write(data)
-   client.write(separator)
-
-   async.setTimeout(10000, function()
-      client.close()
+      async.setTimeout(2000, function()
+         client.close()
+      end)
    end)
-end)
+   table.insert(clients, client)
+end
 
+local start_time = os.time()
 async.go()
+local period = os.time() - start_time
+print("Sent "..sent.." tensors")
+print("Recv "..recv.." tensors")
+print("Speed")
+print(sent/period.." tensor-send/second")
+print(recv/period.."  tensor-recv/second")
+print((recv+sent)/period.." tensors-transmissions/second")
+

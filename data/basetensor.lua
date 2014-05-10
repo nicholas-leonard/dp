d4c7e7522b5766a44debb7fd6bd7085dc1df1391
nicholas-leonard <nick@nikopia.org>
2014-05-10 11:31:24 -0400

    moved BaseTensor.transpose to DataTensor

--- data/basetensor.lua
+++ data/basetensor.lua
@@ -75,29 +75,6 @@ function BaseTensor:cuda()
 end
 
 ---- static methods ----
-function BaseTensor.transpose(axis, new_dim, axes, size, data)
-   -- copy
-   axes = table.copy(axes)
-   local current_dim = _.indexOf(axes, axis)
-   if current_dim == 0 then
-      error("Axis " .. axis .. 'is not in axes ' .. axes)
-   end
-   if new_dim < 0 then
-      new_dim = #axes + 1 - new_dim
-   end
-   if current_dim ~= new_dim then
-      axes[current_dim] = axes[new_dim]
-      axes[new_dim] = axis
-      local new_size = size[new_dim]
-      size[new_dim] = size[current_dim]
-      size[current_dim] = new_size
-      if data then
-         data = data:transpose(new_dim, current_dim)
-      end
-   end
-   return axes, size, data
-end
-
 --returns true if all indices in obj_table are instances of BaseTensor
 --else return false and index of first non-element
 function BaseTensor.areInstances(obj_table)

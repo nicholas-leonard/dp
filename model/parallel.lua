a7d454c3798cb040e8c83342505f3ca1fcb14762
nicholas-leonard <nick@nikopia.org>
2014-07-08 19:34:48 -0400

    async + distributed (w.i.p.) is in its own branch

--- model/parallel.lua
+++ model/parallel.lua
@@ -80,14 +80,3 @@ function Parallel:__tostring__()
    return str
 end
 
------------------------------------------------------------------------
---[[ ParallelProxies ]]--
--- Composed of parallel ModelProxies
--- A composite command is send to station via sendMany() such that 
--- component models can be propagated through concurrently.
--- Receives replies from component commands such that these replies 
--- will potentially use up more bandwith than a more asynchronous 
--- fire and forget model (where forward call the next forward, etc).
--- However the advantage of our approach is that it is easy to implement
--- and provides users with an easier model to build.
-------------------------------------------------------------------------

dda104fbe011e497aabd90e1d20a1da27e05b5e3
daydreamt <uagui@student.kit.edu>
2014-08-31 16:58:55 +0200

    Add recurrent data structures.

--- model/recurrent.lua
+++ model/recurrent.lua
@@ -4,10 +4,8 @@
 -- skip and recurrent connections.
 -- gets passed an additional connections table in addition to models
 -- where each connection is a table with keys: source, target, and isRecurrent.
--- TODO: Subclass sequential or container?
--- TODO: Most methods are still copy pasted
--- TODO: I think most of what you wrote into container should go in here.
--- container should maybe just have the models in the topologically sorted order.
+-- TODO: Yes, this should be a container subclass, but should sequential subclass this one?
+-- TODO: Most methods are still mostly copy pasted
 ------------------------------------------------------------------------
 local Recurrent, parent = torch.class("dp.Recurrent", "dp.Container")
 Recurrent.isSequential = true
@@ -38,6 +36,7 @@ function Recurrent:__init(config)
       for idx, m in pairs(models) do
          vertices[m:name()] = graph.Node(m:name())
       end
+
       for conn_idx, conn in pairs(connections) do
          -- TODO: I would use unpack, but I don't know how to specify multiple types
          -- per model.
@@ -55,20 +54,6 @@ function Recurrent:__init(config)
                "Layer names must be strings or 'dp.ObjectID'")
          end
 
-         --[[
-         source, target,  isRecurrent = xlua.unpack(
-            {conn},
-            'Connections',
-            'Connections between layers',
-            -- TODO: Allow strings as well as dp.ObjectID as allowed types
-            {arg='source', req=true,
-             help='the layer the connection starts from'},
-            {arg='target', req=true,
-             help='the layer the connection goes to'},
-            {arg='isRecurrent', type=boolean, req=false, default=false,
-             help='Whether that connection should be recurrent. Defaults to' ..
-                'false, unless it is from a layer to itself'})
-         --]]
          if source == target then
             isRecurrent = True
          end
@@ -84,14 +69,57 @@ function Recurrent:__init(config)
 
       -- Assert the resulting structure is a DAG and keep the models in
       -- topologically sorted order --TODO: Fix in graph library for all cases
+      -- TODO: More checks about the graph structure here (ie no lone nodes)
       local sortedmodels , __ , __ = g:topsort()
+      -- but first map the nodes to their ids
+      models = {}
+      for idx, node in pairs(sortedmodels) do
+         -- it is an object ID because that is what node.lua expect us to have
+         table.insert(models, dp.ObjectID:create(node:label()))
+      end
+      -- save the graph
+      self._g = g --TODO: Or not. Not needed anymore.
 
-      models = sortedmodels
    end
 
    -- pass the models in topologically sorted order to container.
    config.models = models
    parent.__init(self, config)
+
+   -- use the graph to initialize an appropriate view structure
+
+   -- every graph has an input and output view, which I will save in tables for now
+   -- the lookup key is the name of the model
+   local incoming = {}
+   local outgoing = {}
+
+   -- TODO: As far as I can tell, the graph library has no nice way of
+   -- traversing the nodes once so we are using raw node ids instead.
+   -- TODO: Really do so once we get the debugging finished :P
+   for __, node in pairs(models) do
+      assert(torch.type(node) == 'dp.ObjectID', "Wrong node type.")
+      incoming[node:name()] = {}
+      outgoing[node:name()] = {}
+   end
+
+   for idx, edge in pairs(self._connections) do
+      local source = edge['source']:name()
+      local target = edge['target']:name()
+
+      if not incoming[target] then
+         incoming[target] = {}
+      end
+
+      if not outgoing[source] then
+         outgoing[source] = {}
+      end
+      table.insert(incoming[target], source)
+      table.insert(outgoing[source], target)
+   end
+
+   --TODO: Remove when stable and no longer needs debugging.
+   self._incoming = incoming
+   self._outgoing = outgoing
 
 end
 
@@ -99,11 +127,13 @@ function Recurrent:setup(config)
    parent.setup(self, config)
    config.container = self
    for i, model in ipairs(self._models) do
-      config.id = self:id():create('s'..i)
+      --TODO: Think about how to modify.
+      config.id = self:id():create('r'..i)
       model:setup(config)
    end
 end
 
+-- The forward and backward gradients
 function Recurrent:_forward(carry)
    local input = self.input
    for i=1,#self._models do
@@ -129,15 +159,27 @@ end
 function Recurrent:inputType(input_type)
    if not input_type then
       assert(#self._models > 1, "No models to get input type")
+      -- the first model in the topologically sorted order is automatically
+      -- the root node
       return self._models[1]:inputType()
    end
 end
 
 function Recurrent:outputType(output_type)
+   -- TODO: I think the inverse of the model graph must be some sort of DAG too
+   -- as you must only have one output node of the whole thing.
    if not output_type then
       assert(#self._models > 1, "No models to get input type")
       return self._models[#self._models]:outputType()
    end
+end
+
+function Recurrent:connections()
+   return self._connections
+end
+
+function Recurrent:nodes()
+   return self._models
 end
 
 function Recurrent:__tostring__()

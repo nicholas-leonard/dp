b4bffa1694bbfb800ae904bfccd24f56455dfe30
nicholas-leonard <nick@nikopia.org>
2014-11-12 00:17:00 -0500

    RecurrentVisitorChain fix

--- visitor/recurrentvisitorchain.lua
+++ visitor/recurrentvisitorchain.lua
@@ -21,6 +21,7 @@ function RecurrentVisitorChain:__init(config)
    config.name = config.name or 'recurrentvisitorchain'
    parent.__init(self, config)
    self._visit_interval = visit_interval
+   self._force_visit = false
    self._n_visit = 0
 end
 
@@ -28,16 +29,22 @@ function RecurrentVisitorChain:setup(config)
    parent.setup(self, config)
    -- published by SentenceSampler
    self._mediator:subscribe('doneSequence', self, 'doneSequence')
+   self._mediator:subscribe('doneEpoch', self, 'doneEpoch')
 end
 
 function RecurrentVisitorChain:doneSequence()
    self._force_visit = true
 end
 
+function RecurrentVisitorChain:doneEpoch()
+   self._n_visit = 0
+end
+
 function RecurrentVisitorChain:_visitModel(model)
    self._n_visit = self._n_visit + 1
    if self._force_visit or self._n_visit == self._visit_interval then
       parent._visitModel(self, model)
+      self._n_visit = 0
       self._force_visit = false
    end
 end

a550af18066b1e94b2691c3879353a11a757573c
nicholas-leonard <nick@nikopia.org>
2014-11-23 23:14:27 -0500

    SVHN dataset can use extra dataset and state of the art cross-validation

--- data/svhn.lua
+++ data/svhn.lua
@@ -51,7 +51,7 @@ function Svhn:__init(config)
       {arg='scale', type='table', default=false,
        help='bounds to scale the values between'},
       {arg='download_url', type='string',
-       default='http://data.neuflow.org/data/svhn.t7.tgz',
+       default='http://torch7.s3-website-us-east-1.amazonaws.com/data/svhn.t7.tgz',
        help='URL from which to download dataset if not found on disk.'},
       {arg='load_all', type='boolean',
        help='Load all datasets : train, valid, test.', default=true},
@@ -92,9 +92,13 @@ function Svhn:_loadTrainValid(file_name, valid_ratio, valid_per_class)
       name=self._name, url=self._download_url,
       decompress_file=file_name, data_dir=self._data_path
    }
-   local data = torch.load(doc_path,'ascii'):float() -- was ByteTensor
-   local inputs = train.X:transpose(3,4)
-   local targets = train.y:view(-1)
+   local data = torch.load(doc_path,'ascii') 
+   local inputs = data.X:transpose(3,4):float() -- was ByteTensor
+   data.X = nil
+   collectgarbage()
+   local targets = data.y:view(-1):int() --was DoubleTensor
+   data = nil
+   collectgarbage()
    
    local tstart, vstop = self._train_start, self._valid_stop
    
@@ -141,12 +145,12 @@ function Svhn:_loadTrainValid(file_name, valid_ratio, valid_per_class)
       local valid_targets = self._target_tensor:narrow(1, vstart, nValid)
       
       local vstart_, tstart_ = 1, 1
-      for class, indices in ipairs(class_indices) do
-         local cidx = torch.LongTensor(indices)
+      for class, cindices in ipairs(class_indices) do
+         local cidx = torch.LongTensor(cindices)
          -- valid
          local vidx = cidx:narrow(1, 1, valid_per_class)
          local vinput = valid_inputs:narrow(1, vstart_, valid_per_class)
-         local vtarget = valid_inputs:narrow(1, vstart_, valid_per_class)
+         local vtarget = valid_targets:narrow(1, vstart_, valid_per_class)
          vinput:index(inputs, 1, vidx)
          vtarget:index(targets, 1, vidx)
          vstart_ = vstart_ + valid_per_class
@@ -156,14 +160,14 @@ function Svhn:_loadTrainValid(file_name, valid_ratio, valid_per_class)
          local tidx = cidx:narrow(1, valid_per_class+1, tsize)
          self._fbuffer:index(inputs, 1, tidx)
          self._ibuffer:index(targets, 1, tidx)
-         
-         local tindices = indices:narrow(1, tstart_, valid_per_class)
+
+         local tindices = indices:narrow(1, tstart_, tsize)
          self._input_tensor:indexCopy(1, tindices, self._fbuffer)
          self._target_tensor:indexCopy(1, tindices, self._ibuffer)
          tstart_ = tstart_ + tsize
       end
-      assert(vstart_ == valid_targets:size(1) + 1)
-      assert(tstart_ == nTrain - 1)
+      assert(vstart_ == valid_targets:size(1) + 1, vstart_ .. " ~= " .. valid_targets:size(1) + 1)
+      assert(tstart_ == nTrain + 1, tstart_ .. "~=" .. nTrain + 1)
       self._train_start = tstart + nTrain
       self._valid_stop = vstart - 1
    end
@@ -178,9 +182,9 @@ function Svhn:loadTrainValid()
                   
    if self._use_extra then
       nSample = nSample + 531131
-      nValid = nValid + self._valid_ratio 
-                        and math.floor(531131*self._valid_ratio[2])
-                        or self._valid_per_class[2] * 10
+      nValid = nValid + (self._valid_ratio
+                        and math.floor(531131*self._valid_ratio[2]) 
+                        or self._valid_per_class[2] * 10)
    end
    
    self._input_tensor = torch.FloatTensor(nSample, 3, 32, 32):fill(-1)
@@ -202,13 +206,13 @@ function Svhn:loadTrainValid()
    if self._use_extra then
       self:_loadTrainValid(
          self._extra_file, 
-         self._valid_ratio = self._valid_ratio[2], 
-         self._valid_per_class = self._valid_per_class[2]
+         self._valid_ratio and self._valid_ratio[2], 
+         self._valid_per_class and self._valid_per_class[2]
       )
    end
    
-   assert(math.min(train_inputs:min(), train_targets:min()) > -1)
-   assert(math.min(valid_inputs:min(), valid_targets:min()) > -1)
+   assert(self._input_tensor:min() > -1)
+   assert(self._target_tensor:min() > -1)
    
    self:setTrainSet(
       self:createDataSet(
